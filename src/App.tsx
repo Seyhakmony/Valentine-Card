1bb410c18bc29f33ef19f9e9072c458fe96562ce
Seyhakmony <158637575+Seyhakmony@users.noreply.github.com>
2024-02-08 16:51:36 -0500

    Update App.tsx
    f

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
   const [noCount, setNoCount] = useState(0);
   const [yesPressed, setYesPressed] = useState(false);
   const [noButtonPosition, setNoButtonPosition] = useState({
-    top: "700px",
+    top: "750px",
   });
 
   const yes = noCount * 20 + 16;
