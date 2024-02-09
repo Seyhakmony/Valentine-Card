41c297af350d9be7a19aabeeff57be5507b8ce28
Seyhakmony <158637575+Seyhakmony@users.noreply.github.com>
2024-02-08 22:01:05 -0500

    Update App.tsx
    f

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,23 @@ const phrase = [
   "No",
   "Are you Sure?",
   "Really Sure?",
-  "제발...",
-  "I will treat you well",
-  "I'm going to cry",
-  "You're breaking my heart ;(",
-  "제발...",
-  "Give me a chance",
-  "1 chance",
-  "I even pay for your ice cream",
-  "You know what, I dont need you",
-  "I'm better without you in my life",
-  "Actually, never mind, I need you",
-  "So please give me a chance",
+  // "제발...",
+  // "I will treat you well",
+  // "I'm going to cry",
+  // "You're breaking my heart ;(",
+  // "제발...",
+  // "Give me a chance",
+  // "1 chance",
+  // "I even pay for your ice cream",
+  // "You know what, I dont need you",
+  // "I'm better without you in my life",
+  // "Actually, never mind, I need you",
+  // "So please give me a chance",
+  "You know I use to play D2",
+  "I kind of droll a little",
+  "I will always pay for your ice cream",
+  "One Chance",
+  "Son Heung-min is the goat",
 ];
 
 const getRandomPosition = () => {
@@ -29,7 +34,7 @@ function App() {
   const [noCount, setNoCount] = useState(0);
   const [yesPressed, setYesPressed] = useState(false);
   const [noButtonPosition, setNoButtonPosition] = useState({
-    top: "750px",
+    top: "700px",
   });
 
   const yes = noCount * 20 + 16;
@@ -49,7 +54,8 @@ function App() {
         <>
           <img
             alt="this could be us"
-            src="https://c.tenor.com/nfhDavLGkZEAAAAC/tenor.gif"
+            // src="https://c.tenor.com/nfhDavLGkZEAAAAC/tenor.gif"
+            src="https://media1.tenor.com/m/qJaeM0KuE5UAAAAC/cat-cats.gif"
           />
           <div className="Text">You won't regret this.</div>
         </>
@@ -58,7 +64,8 @@ function App() {
           <img
             className="flower"
             alt="end game start"
-            src="https://c.tenor.com/g3Q53yKDdloAAAAd/tenor.gif"
+            // src="https://c.tenor.com/g3Q53yKDdloAAAAd/tenor.gif"
+            src="https://media.tenor.com/OzNPe52enwwAAAAi/mochi-cat-love.gif"
           />
           <div
             className="question"
