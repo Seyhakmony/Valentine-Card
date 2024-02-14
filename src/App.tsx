8f28d01e21ce1cb117fa8fe57ff6e65770155199
Seyhakmony <158637575+Seyhakmony@users.noreply.github.com>
2024-02-14 13:42:45 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -174,7 +174,7 @@ function App() {
   const [noCount, setNoCount] = useState(0);
   const [yesPressed, setYesPressed] = useState(false);
   const [noButtonPosition, setNoButtonPosition] = useState({
-    top: "600px",
+    top: "620px",
   });
   const [currentImg, setCurrentImg] = useState(firstImg);
 
